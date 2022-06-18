9abdbef9cb10ce718c1bbb71d156bef07f6262d0
PostnicovaY <oceana91@rambler.ru>
2022-06-18 23:23:46 +0300

    Lesson_4

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,15 +1,37 @@
+from rest_framework import status
+from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.renderers import JSONRenderer
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
+from .filters import ProjectFilter, TODOFilter
 from .models import TODO, Project
 from .serializers import ProjectModelSerializer, TODOModelSerializer
+
+
+class ProjectLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 10
+
+
+class TODOLimitOffsetPagination(LimitOffsetPagination):
+    default_limit = 20
 
 
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectLimitOffsetPagination
+    filterset_class = ProjectFilter
 
 
 class TODOModelViewSet(ModelViewSet):
     queryset = TODO.objects.all()
     serializer_class = TODOModelSerializer
+    pagination_class = TODOLimitOffsetPagination
+    filterset_class = TODOFilter
+
+    def destroy(self, request, *args, **kwargs):
+        project = self.get_object()
+        project.active = False
+        project.save()
+        return Response(status=status.HTTP_202_ACCEPTED)
