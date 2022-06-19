aee101728ccfcfde92ba3667db3757a4776c531b
PostnicovaY <oceana91@rambler.ru>
2022-06-20 00:47:16 +0300

    Lesson_7

--- frontend/src/components/menu.js
+++ frontend/src/components/menu.js
@@ -1,8 +1,31 @@
 import React from "react";
-import { BrowserRouter, Route, Link, Switch, Redirect } from 'react-router-dom'
-import ProjectList from "./project";
+import { Link } from 'react-router-dom';
+import './auth';
 
-const MenuList = ({ }) => {
+// const MenuList = () => {
+//     return (
+//         <header>
+//             <ul className="menu">
+//                 <li>
+//                     <Link to='/'>Все пользователи</Link>
+//                 </li>
+//                 <li>
+//                     <Link to='/projects'>Проекты</Link>
+//                 </li>
+//                 <li>
+//                     <Link to='/todo'>TODO</Link>
+//                 </li>
+//                 {/* <li>
+//                     {is_auth ?
+//                         <button onClick={() => logout()}>Logout</button> :
+//                         <Link to='/login'>Login</Link>}
+//                 </li> */}
+
+//             </ul >
+//         </header >
+//     )
+// }
+export default function MenuList(props) {
     return (
         <header>
             <ul className="menu">
@@ -15,8 +38,12 @@ const MenuList = ({ }) => {
                 <li>
                     <Link to='/todo'>TODO</Link>
                 </li>
-            </ul>
+                <li>
+                    {props.is_auth ?
+                        <button onClick={props.logout}>Hello, {props.username}! Logout</button> :
+                        <Link to='/login'>Login</Link>}
+                </li>
+            </ul >
         </header >
     )
 }
-export default MenuList
